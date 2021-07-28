2cfcff25b84e1a13a7cc98a87652a1b585cc2ffa
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-07-28 22:31:14 +0900

    update

---
+++ src/components/Input.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{ searchText }}</p>
+    <input v-model="searchText">
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+  name: 'Input',
+  setup() {
+    const searchText = ref();
+    return {
+      searchText,
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
